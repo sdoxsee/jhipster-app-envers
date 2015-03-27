497daeb86b75082fdea650b24e2ce77303a36993
Stephen Doxsee <stephen@doxsee.org>
2015-03-27 14:25:28 -0400

    Added entity Foo

---
+++ src/main/java/com/mycompany/myapp/domain/Foo.java
@@ -0,0 +1,72 @@
+package com.mycompany.myapp.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Foo.
+ */
+@Entity
+@Table(name = "T_FOO")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Foo implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "name", length = 20, nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Foo foo = (Foo) o;
+
+        if ( ! Objects.equals(id, foo.id)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Foo{" +
+                "id=" + id +
+                ", name='" + name + "'" +
+                '}';
+    }
+}
